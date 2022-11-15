b3e1c58d3b747c13541022a93a84acbe9c146cf5
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-11-15 12:00:06 +0900

    NextJS_static Pre Rendering

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function About() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
