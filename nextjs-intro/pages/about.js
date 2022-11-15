005c9e6ee62e86c6d03a690ef6de602bf7628501
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-11-15 14:07:44 +0900

    NextJS_Routing

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import NavBar from "../components/NavBar";
 
 export default function About() {
-  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+      <NavBar />
+      <h1>About us</h1>
     </div>
   );
 }
