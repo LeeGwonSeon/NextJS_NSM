6234fc494955f28293358e94f9c39f6b607f8d51
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-11-29 16:16:52 +0900

    NextJS_Catch All

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,14 +1,26 @@
-import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
-export default function Home({results}) {
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4 onClick={() => onClick(movie.id)}>{movie.original_title}</h4>
         </div>
       ))}
       <style jsx>{`
@@ -37,7 +49,9 @@ export default function Home({results}) {
 }
 
 export async function getServerSideProps() {
-  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
   return {
     props: {
       results,
